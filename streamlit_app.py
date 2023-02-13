6446df5d5a2b85ea560e57eaada0d6185d388404
Aitorhermida <45969514+Aitorhermida@users.noreply.github.com>
2023-02-13 20:27:41 +0100

    Update streamlit_app.py
    added line

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
